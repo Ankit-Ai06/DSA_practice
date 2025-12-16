c088a9e8ddddb95ff46424260325d98677da27c0
Ankit <anky.g13@gmail.com>
2025-12-16 15:29:50 +0530

    Time: 6 ms (29.84%) | Memory: 45.8 MB (62.03%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        // Negative numbers are not palindrome
+        if (x < 0) return false;
+
+        String s = String.valueOf(x);
+        int i = 0, j = s.length() - 1;
+
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
